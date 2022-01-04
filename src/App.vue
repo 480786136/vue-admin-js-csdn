1bc4db9a62a7698537108e2dbf47d57390762beb
480786136 <46239248+480786136@users.noreply.github.com>
2022-01-04 17:39:18 +0800

    更新App.vue

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,6 @@
 <template>
   <div id="nav">
-    <router-link to="/">AAAAHome</router-link> |
+    <router-link to="/">AAAHome</router-link> |
     <router-link to="/about">About</router-link>
   </div>
   <router-view />
